4cf96e25d90f1993948055e3bab0eb71b4495988
kurogami20 <138148504+kurogami20@users.noreply.github.com>
2025-02-28 11:11:35 +0100

    Update Client.js
    on peut récupérer le et toutes ses chambres réservé

--- back/src/controllers/Client.js
+++ back/src/controllers/Client.js
@@ -7,22 +7,41 @@ const clientController = {
     const bookSchema = z.object({
       lastname: z.string().nonempty(),
       date: z.number().positive().min(1),
-      id_client: z.number().positive().min(1),
-      id_room: z.number().positive().min(1).optional(),
+      client_id: z.number().positive().min(1),
+      room_id: z.number().positive().min(1).optional(),
     });
 
     const verif = bookSchema.parse(data);
     if (!verif.id_room) {
-      const book = await Client.findOne({
-        where: { id: verif.id_client, lastname: verif.lastname },
-        include: [
-          {
-            association: "room",
-          },
-        ],
+      // on vérifie si la réservation existe
+      const book = await Booking.findAll({
+        where: { date: verif.date, client_id: verif.client_id },
+        attributes: ["room_id"],
       });
 
-      res.status(200).json(book);
+      if (!book) {
+        throw new Error();
+      }
+
+      console.log(book);
+      // on envoie la réservation (client plus les chambres)
+      const client = await Client.findByPk(verif.client_id);
+      const rooms = await Promise.all(
+        book.map(async (b) => {
+          return await Client.findOne({
+            where: { id: verif.client_id, lastname: verif.lastname },
+            attributes: [],
+            include: [
+              {
+                association: "room",
+                where: { id: b.room_id },
+              },
+            ],
+          });
+        })
+      );
+      rooms.unshift(client);
+      res.status(200).json(rooms);
     } else {
       const book = await Client.findOne({
         where: { id: verif.id_client, lastname: verif.lastname },
